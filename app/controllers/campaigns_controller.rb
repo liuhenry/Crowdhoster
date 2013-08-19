a2b125279c089ac1b4fced2489511007b01671be
Henry Liu <henry@liuhenry.com>
2013-08-19 09:13:14 -0700

    Move update_item code into partial for reuse, lighten background on individual update view, update some links

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -8,7 +8,7 @@ class CampaignsController < ApplicationController
   # and makes it available to all routes
 
   def home
-    @updates = @campaign.updates.order("publish_date ASC").select("title, slug, body, publish_date").limit(3)
+    @updates = @campaign.updates.order("publish_date DESC").select("title, slug, body, publish_date").limit(4)
     @update_count = @campaign.updates.count()
     render 'theme/views/campaign'
   end
